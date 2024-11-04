e34ca3080093211496e6c1a991f615ecfac70f4d
Shreemadhi Babu Rajendra Prasad <shreemadhi.baburajendrapra@ucdconnect.ie>
2024-11-04 00:04:40 +0000

    Adding practical 3 files

---
+++ practical03/use_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i1 = 101;
+    int i2 = 2000;
+    float fl1 = 11.11;
+    float fl2 = 21.2222;
+    //printf("I would like the value of i1, that value is %d\n",i1);
+    //printf("The two ints are %d %d and two floats are %f %f", i1, i2, fl1, fl2);
+    printf("Two ints are %d %d and two floats are %.4f %.4f\n", i1, i2, fl1, fl2);
+    return 0;
+}
